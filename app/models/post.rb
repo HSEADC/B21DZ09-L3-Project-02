477c3bd5987cf7796337f42364657d15352c73f4
ntizish <dan.perednja@gmail.com>
2023-10-08 20:10:18 +0300

    Added: About page, Navigation bar, Footer

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+    validates :user_id, :presence => true
+    validates :title, :presence => true
+    validates :artist, :presence => true
+	validates :description, :presence => true, :length => { :minimum => 10 }
+    validates :body, :presence => true, :length => { :minimum => 30 }
+
+    has_many :comments
+end

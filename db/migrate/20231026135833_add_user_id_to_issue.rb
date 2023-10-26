2e7b17a655e62cec1d18c23b7c517566bf83e875
ntizish <dan.perednja@gmail.com>
2023-10-26 17:34:07 +0300

    User added

---
+++ db/migrate/20231026135833_add_user_id_to_issue.rb
@@ -0,0 +1,5 @@
+class AddUserIdToIssue < ActiveRecord::Migration[7.0]
+  def change
+    add_column :issues, :user_id, :integer
+  end
+end
---
+++ db/migrate/20231026135913_add_user_id_to_artist.rb
@@ -0,0 +1,5 @@
+class AddUserIdToArtist < ActiveRecord::Migration[7.0]
+  def change
+    add_column :artists, :user_id, :integer
+  end
+end

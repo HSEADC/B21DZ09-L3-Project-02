a477708aac8bdbed7b6e98f60a6240b5f1381110
ntizish <dan.perednja@gmail.com>
2023-10-27 18:22:46 +0300

    Subscription added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @color = '#'
-    options = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
-    6.times { @color += options.sample }
+    @subscription = Subscription.new
   end
 
   def about

2e56fff469c04aba82b75d3bcd82d9506cb0b3c7
adc <tetimofeev@edu.hse.ru>
2024-02-07 15:04:16 +0000

    Mailer finished

--- app/mailers/support_mailer.rb
+++ app/mailers/support_mailer.rb
@@ -4,6 +4,7 @@ class SupportMailer < ApplicationMailer
         # @user = params[:user]
         # @url  = 'http://example.com/login'
         # mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+        @support = params[:support]
         post_image = params[:support][:post_image]
         email = params[:support][:email]
         mail(from: "admin@test.com",to: email, subject: "Ваше обращение в поддержку принято")
@@ -11,6 +12,7 @@ class SupportMailer < ApplicationMailer
 
     def admin_support_request_email
 
+        @support = params[:support]
         @post_image = params[:support][:post_image]
         @email = params[:support][:email]
         @comment = params[:support][:comment]
--- app/models/support.rb
+++ app/models/support.rb
@@ -1,2 +1,3 @@
 class Support < ApplicationRecord
+    mount_uploader :post_image, PostImageUploader
 end

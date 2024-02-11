881ebd8ee68b789dbb704386147ab357501d3af0
sasha_shushina <amshushina@edu.hse.ru>
2024-02-11 15:21:38 +0300

    API

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -10,6 +10,27 @@ class Api::V1::PostsController < Api::V1::ApplicationController
       @artist = Artist.find(params[:artist_id])
       @issue = Issue.find(params[:issue_id])
     end
+
+    def create
+      puts "======================="
+      puts "REQUEST FROM FRONTEND"
+      jti  = request.headers["Authorization"]
+      puts "======================="
+
+      @user = User.find_by_jti(jti)
+      @post = @user.posts.new(post_params)
+
+      if @post.save
+        render json: @post.as_json
+      else
+        render json: @post.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+      def post_params
+        params.require(:post).permit(:name, :description, :body, :issue_id, :post_image)
+      end
   
   end
   
--- app/views/api/v1/posts/index.json.jbuilder
+++ app/views/api/v1/posts/index.json.jbuilder
@@ -1,1 +1,5 @@
-json.array! @posts, partial: "api/v1/posts/post", as: :post
+json.set! :jti, User.find_by_email("admin@email.com").jti
+
+json.set! :posts do
+    json.array! @posts, partial: "api/v1/posts/post", as: :post
+end
